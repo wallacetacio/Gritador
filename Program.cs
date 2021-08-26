b86429a18997536bcc95a690003f8c94c3b4be42
Wallace Tácio <wallacett.dev@gmail.com>
2021-08-25 23:59:45 -0300

    Adiciona resolução do exercício Gritador ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoDigitado, textoExibido, nomeDigitado;
+            Console.Write("Por favor, digite seu nome: ");
+            nomeDigitado = Console.ReadLine();
+
+            Console.WriteLine($"Olá {nomeDigitado}. Vou GRITAR o que você digitar.");
+
+            Console.Write("Digite o que quiser: ");
+            textoDigitado = Console.ReadLine();
+
+            textoExibido = textoDigitado.ToUpper();
+
+            Console.WriteLine("Eu grito: ");
+            Console.WriteLine($"{textoExibido} !!!");
+
+            Console.WriteLine($"Você digitou: {textoDigitado}");
+
+        }
+    }
+}
